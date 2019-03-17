4ee11c82d5e6b1523e82f3aaa70db04e9981caf9
xtrojak <trojak@mail.muni.cz>
2019-03-17 10:59:35 +0100

    Added possibility to define order of params (x and y axis)

--- sample.py
+++ sample.py
@@ -21,7 +21,7 @@ def samplePoints(function, params, keys):
 	return results
 
 '''
-python3 sample.py <function-to-sample> <parameters> <output_file>
+python3 sample.py <function-to-sample> <parameters> <output_file> [params order]
 
 where <parameters> is a dictionary of type:
 	"param-name" : [From, To, Number]
@@ -29,14 +29,19 @@ where <parameters> is a dictionary of type:
 	with interval (<From>, <To>) and <Number> samples.
 
 Example:
-	python3 sample.py '(k1)/(k1+2*k2)' '{"k1" : [5, 10, 10], "k2" : [0, 2, 10]}' sampling.svg
+	python3 sample.py '(k1)/(k1+2*k2)' '{"k1" : [5, 10, 10], "k2" : [0, 2, 10]}' sampling.svg 'k1 k2'
 '''
 if __name__ == '__main__':
-	output_file = sys.argv[-1]
-	params = eval(sys.argv[-2])
-	function = sys.argv[-3]
-	
-	keys = list(params.keys())
+	if len(sys.argv) == 5:
+		keys = sys.argv[-1].split()
+		output_file = sys.argv[-2]
+		params = eval(sys.argv[-3])
+		function = sys.argv[-4]
+	else:
+		output_file = sys.argv[-1]
+		params = eval(sys.argv[-2])
+		function = sys.argv[-3]
+		keys = list(params.keys())
 
 	points = samplePoints(function, params, keys)
 	probs = column(points, 2)
